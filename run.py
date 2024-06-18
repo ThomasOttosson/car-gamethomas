42b48c04def63c5ff28d59498178dce278452628
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-06-18 16:31:57 +0000

    initalcommit

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_shot():
+
+    shot = input("please enter your guess")
+
+def show_board(hit,miss,comp):
+
+
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            ch = " _ "
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch = " o "
+            elif place in comp:
+                ch = " O "
+
+            row = row + ch
+            place = place + 1
+
+hit = [21,22]
+miss = [20,24,12,13]
+comp = [23]
+
+get_shot()
+show_board(hit,miss,comp)
