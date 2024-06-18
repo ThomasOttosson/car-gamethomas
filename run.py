cb41ca02f642111b10a995db3bd91e9949b21fc2
ThomasOttosson Ottosson <tomasottosson1@gmail.com>
2024-06-18 17:49:27 +0000

    added more code to battleships game

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
 def get_shot():
 
-    shot = input("please enter your guess")
+    ok = "n"
+    while ok == "n":
+        try:
+            shot = input("please enter your guess")
+            shot = int(shot)
+            if shot < 0 or shot > 99:
+                print("incorrect number, please try again")
+            else:
+                ok = "y"
+                break
+        except:
+            print("incorrect entry, please enter again")
+    return shot
 
 def show_board(hit,miss,comp):
-
+    print("            battleships    ")
+    print("     0  1  2  3  4  5  6  7  8  9")
 
     place = 0
     for x in range(10):
@@ -19,10 +32,11 @@ def show_board(hit,miss,comp):
 
             row = row + ch
             place = place + 1
+            print(x," ",row)
 
 hit = [21,22]
 miss = [20,24,12,13]
 comp = [23]
 
-get_shot()
+shot = get_shot()
 show_board(hit,miss,comp)
